3e218f6f3dc129828eeb809cd78747c2ec15fef9
Andrey <mezentsew@mail.ru>
2024-04-29 20:13:34 +0300

    Created two modules models.py from applications: materials & users. Made first migrations.

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    """Модель учебного курса"""
+
+    name = models.CharField(max_length=200, verbose_name="Название")
+    preview = models.ImageField(upload_to="course_preview", verbose_name="Изображение", **NULLABLE)
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+
+class Lesson(models.Model):
+    """Модель урока"""
+
+    course = models.ForeignKey(Course, verbose_name="Курс", related_name="lesson", on_delete=models.SET_NULL,**NULLABLE)
+    name = models.CharField(max_length=255, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание")
+    preview = models.ImageField(upload_to="lesson_preview",verbose_name="Изображение",**NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
--- users/models.py
+++ users/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from materials.models import Course, Lesson
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    """Модель пользователя"""
+
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Почта")
+    avatar = models.ImageField(upload_to="users/", verbose_name="Аватар", **NULLABLE)
+    phone_number = models.CharField(max_length=35, verbose_name="Номер телефона", **NULLABLE)
+    city = models.CharField(max_length=150, verbose_name="Город", **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+
+
+class Payment(models.Model):
+    """Модель платежа"""
+    class PaymentType(models.TextChoices):
+        CASH = 'cash', 'наличные'
+        SPENDING = 'spending', 'перевод на счет'
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='user', verbose_name='Пользователь', **NULLABLE)
+    date = models.DateField(auto_now=True, verbose_name='Дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Оплаченный курс')
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='Оплаченный урок', **NULLABLE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Сумма оплаты')
+    payment_type = models.CharField(max_length=32, choices=PaymentType.choices, verbose_name='Способ оплаты')
+
+    class Meta:
+        verbose_name = 'Платеж'
+        verbose_name_plural = 'Платежи'
+        ordering = ('user', 'date')
