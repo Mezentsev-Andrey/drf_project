d8b06e561755edd82a6e5f26934319ad9da36dcd
Andrey <mezentsew@mail.ru>
2024-04-29 20:44:31 +0300

    Added new changed information in modules: sirialezers.py, views.py, urls.py, settings.py.

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'materials',
     'users',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -134,9 +135,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
-    # Use Django's standard 'django.contrib.auth' permissions,
-    # or allow read-only access for unauthenticated users.
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+
+
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.IsAuthenticated',
     ]
+
 }
--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+import django_filters.rest_framework
+from rest_framework.filters import SearchFilter
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from users.models import Payment, User
+from users.serializers import UserSerializer, PaymentSerializer
+
+
+class UsersAPIViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class PaymentlListView(ListAPIView):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    filter_backends = [SearchFilter, django_filters.rest_framework.DjangoFilterBackend]
+
+    filterset_fields = ['course', 'lesson', 'payment_type']
+    ordering_fields = ['date']
